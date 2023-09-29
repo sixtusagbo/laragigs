d714b91840b09a0f1c489acd1bf37c01feddb93b
sixtusagbo <miracleagbosixtus@gmail.com>
2023-09-29 02:02:12 +0100

    Refresh on some basic routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,12 @@ Route::get('/hello', function () {
     return response('<h1>Hello World!</h1>', 200)
         ->header('Content-Type', 'text/plain')
         ->header('foo', 'bar');
+});
+
+Route::get('/posts/{id}', function ($id) {
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
+
+Route::get('/search', function (Request $request) {
+    return $request->name . ' ' . $request->city;
 });
